4cfa96053de5a79fdf7511382b9235c071170766
Olha Krapivina <olga.krapivina.zireael@gmail.com>
2022-04-14 01:47:59 +0300

    commit #1

---
+++ js/notes.js
@@ -0,0 +1,113 @@
+const notes = [{
+    "id": 1,
+    "title": "Rock-a-Bye Baby",
+    "preview": "Toxic effect of unsp substance, accidental, init",
+    "description": "Toxic effect of unspecified substance, accidental (unintentional), initial encounter",
+    "date": "7/1/2021",
+    "isPopular": true
+   }, {
+    "id": 2,
+    "title": "Fans",
+    "preview": "Sleep disorder not due to a sub or known physiol cond, unsp",
+    "description": "Sleep disorder not due to a substance or known physiological condition, unspecified",
+    "date": "3/23/2022",
+    "isPopular": true
+   }, {
+    "id": 3,
+    "title": "Zorn's Lemma",
+    "preview": "Poisoning by cocaine, assault, initial encounter",
+    "description": "Poisoning by cocaine, assault, initial encounter",
+    "date": "3/12/2022",
+    "isPopular": true
+   }, {
+    "id": 4,
+    "title": "Any Day Now",
+    "preview": "Strain of musc/fasc/tend at wrs/hnd lv, left hand, sequela",
+    "description": "Strain of other specified muscles, fascia and tendons at wrist and hand level, left hand, sequela",
+    "date": "8/10/2021",
+    "isPopular": false
+   }, {
+    "id": 5,
+    "title": "Will Penny",
+    "preview": "Pregnancy with inconclusive fetal viability, fetus 3",
+    "description": "Pregnancy with inconclusive fetal viability, fetus 3",
+    "date": "8/14/2021",
+    "isPopular": false
+   }, {
+    "id": 6,
+    "title": "And Soon the Darkness",
+    "preview": "Supervision of elderly primigravida and multigravida",
+    "description": "Supervision of elderly primigravida and multigravida",
+    "date": "10/21/2021",
+    "isPopular": false
+   }, {
+    "id": 7,
+    "title": "Synchromy",
+    "preview": "Toxic effect of venom of snake, accidental, sequela",
+    "description": "Toxic effect of venom of other snake, accidental (unintentional), sequela",
+    "date": "2/23/2022",
+    "isPopular": false
+   }, {
+    "id": 8,
+    "title": "Girl From Nowhere, The",
+    "preview": "Other dystrophies primarily involving the sensory retina",
+    "description": "Other dystrophies primarily involving the sensory retina",
+    "date": "9/21/2021",
+    "isPopular": true
+   }, {
+    "id": 9,
+    "title": "Is It Easy to be Young?",
+    "preview": "Bitten by other hoof stock",
+    "description": "Bitten by other hoof stock",
+    "date": "8/16/2021",
+    "isPopular": false
+   }, {
+    "id": 10,
+    "title": "Case of You, A",
+    "preview": "Burn of third degree of unspecified thigh, sequela",
+    "description": "Burn of third degree of unspecified thigh, sequela",
+    "date": "10/20/2021",
+    "isPopular": false
+   }, {
+    "id": 11,
+    "title": "Roll Bounce",
+    "preview": "Encounter for antineoplastic chemotherapy and immunotherapy",
+    "description": "Encounter for antineoplastic chemotherapy and immunotherapy",
+    "date": "8/16/2021",
+    "isPopular": false
+   }, {
+    "id": 12,
+    "title": "Bliss (Mutluluk)",
+    "preview": "Congenital diverticulum of bladder",
+    "description": "Congenital diverticulum of bladder",
+    "date": "11/6/2021",
+    "isPopular": true
+   }, {
+    "id": 13,
+    "title": "August Mordum's Underground",
+    "preview": "Occlusion and stenosis of bilateral carotid arteries",
+    "description": "Occlusion and stenosis of bilateral carotid arteries",
+    "date": "10/30/2021",
+    "isPopular": true
+   }, {
+    "id": 14,
+    "title": "Top Secret!",
+    "preview": "Other contact with crocodile, sequela",
+    "description": "Other contact with crocodile, sequela",
+    "date": "6/27/2021",
+    "isPopular": false
+   }, {
+    "id": 15,
+    "title": "Occupant",
+    "preview": "Other calcification of muscle, left upper arm",
+    "description": "Other calcification of muscle, left upper arm",
+    "date": "6/17/2021",
+    "isPopular": false
+   }, {
+    "id": 16,
+    "title": "Lady in Cement",
+    "preview": "2-part nondisp fx of surg nk of r humer, 7thK",
+    "description": "2-part nondisplaced fracture of surgical neck of right humerus, subsequent encounter for fracture with nonunion",
+    "date": "2/18/2022",
+    "isPopular": true
+   }]
