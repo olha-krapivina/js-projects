e414977b493636ac084e8784767cefcc0a59b916
Olha Krapivina <olga.krapivina.zireael@gmail.com>
2022-04-15 23:26:15 +0300

    commit #7
    added sticker "popular"

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@
     const container = document.querySelector("#container");
 
     const createCardStructure = ( {title, date, preview} ) => `
+        <div class="popular _card-design">
+            <p><b>Popular</b></p>
+        </div>
         <div class="card-header">
             <div class="card-title _card-design"><span><b>${title}</b></span></div>
             <div class="card-date _card-design"><span><b>${date}</b></span></div>
