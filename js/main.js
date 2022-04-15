22c21e58912c88a5c9ed9ef2123e050ae3d480a0
Olha Krapivina <olga.krapivina.zireael@gmail.com>
2022-04-15 16:57:49 +0300

    commit #6
    cards were added using the js

--- js/main.js
+++ js/main.js
@@ -3,13 +3,29 @@
 (function (){
     const container = document.querySelector("#container");
 
-    // const createCardStructure = 
+    const createCardStructure = ( {title, date, preview} ) => `
+        <div class="card-header">
+            <div class="card-title _card-design"><span><b>${title}</b></span></div>
+            <div class="card-date _card-design"><span><b>${date}</b></span></div>
+        </div>
+
+        <div class="card-preview-description _card-design">
+            <p>${preview}</p>
+        </div>
+
+        <button class="card-btn">More...</button>
+    `;
 
     const render = () => {
-        const blocks = notes.map((card) => {
+        const cards = notes.map((card) => {
             const cardBlock = document.createElement("div");
             cardBlock.className = "card";
-        }); 
+
+            cardBlock.innerHTML = createCardStructure(card);
+
+            return cardBlock;
+        });
+        container.append(...cards);
     };
     render();
 })();
